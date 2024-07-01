0a8a07e0989982618dc6ce8fb58b31954a86c6b0
tesinitsyn <sitiev174@gmail.com>
2024-07-01 22:46:41 +0300

    completed controller

--- src/main/java/com/tesinitsyn/crocdayonepractise/controller/SearchEmployeeController.java
+++ src/main/java/com/tesinitsyn/crocdayonepractise/controller/SearchEmployeeController.java
@@ -1,5 +1,6 @@
 package com.tesinitsyn.crocdayonepractise.controller;
 
+import com.tesinitsyn.crocdayonepractise.dto.EmployeeDto;
 import com.tesinitsyn.crocdayonepractise.entity.Employee;
 import com.tesinitsyn.crocdayonepractise.repository.EmployeeRepository;
 import com.tesinitsyn.crocdayonepractise.utils.EmployeeMapper;
@@ -24,6 +25,13 @@ public class SearchEmployeeController {
     private final EmployeeMapper employeeMapper;
 
 
+    /**
+     * To import EmployeeMapperImpl try to
+     * 1. Maven reload
+     * 2. Maven package
+     * 3. Maven clean
+     * and retry in every order if you can't import it :)
+     */
     public SearchEmployeeController(SearchQueryProcessor queryProcessor, EmployeeRepository employeeRepository, EmployeeMapper employeeMapper) {
         this.queryProcessor = queryProcessor;
         this.employeeRepository = employeeRepository;
@@ -31,7 +39,7 @@ public class SearchEmployeeController {
     }
 
     @GetMapping
-    public ResponseEntity<Employee> search(@RequestParam(value = "search", required = false) String search) {
+    public ResponseEntity<EmployeeDto> search(@RequestParam(value = "search", required = false) String search) {
         var employee = (Employee) null;
         if (search != null) {
             var predicate = queryProcessor.processQuery(search);
@@ -40,7 +48,7 @@ public class SearchEmployeeController {
         if (Objects.isNull(employee)) {
             return ResponseEntity.notFound().build();
         } else {
-            return ResponseEntity.ok(employee);
+            return ResponseEntity.ok(employeeMapper.employeeToEmployeeDto(employee));
         }
     }
 }
