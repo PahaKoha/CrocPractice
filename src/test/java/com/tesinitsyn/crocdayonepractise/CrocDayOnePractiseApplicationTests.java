74130d31d5994af16161194213c38537918779e9
tesinitsyn <sitiev174@gmail.com>
2024-07-01 23:25:39 +0300

    refactoring

--- src/test/java/com/tesinitsyn/crocdayonepractise/CrocDayOnePractiseApplicationTests.java
+++ src/test/java/com/tesinitsyn/crocdayonepractise/CrocDayOnePractiseApplicationTests.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+/**
+ * tests not working)
+ * TODO: fix test containers
+ */
 @SpringBootTest
 @ActiveProfiles("test")
 class CrocDayOnePractiseApplicationTests {
