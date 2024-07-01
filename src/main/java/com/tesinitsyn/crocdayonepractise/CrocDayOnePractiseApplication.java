6a1fa2a9d8d5e0bd24cc0fa155842fbb725a851e
tesinitsyn <sitiev174@gmail.com>
2024-07-01 21:55:59 +0300

    Initial commit

---
+++ src/main/java/com/tesinitsyn/crocdayonepractise/CrocDayOnePractiseApplication.java
@@ -0,0 +1,13 @@
+package com.tesinitsyn.crocdayonepractise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrocDayOnePractiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrocDayOnePractiseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tesinitsyn/crocdayonepractise/CrocDayOnePractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tesinitsyn.crocdayonepractise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrocDayOnePractiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
