801795031d057c32b0533404429e677fdc55525b
tesinitsyn <sitiev174@gmail.com>
2024-07-01 22:09:59 +0300

    added test containers

--- src/test/java/com/tesinitsyn/crocdayonepractise/CrocDayOnePractiseApplicationTests.java
+++ src/test/java/com/tesinitsyn/crocdayonepractise/CrocDayOnePractiseApplicationTests.java
@@ -2,8 +2,10 @@ package com.tesinitsyn.crocdayonepractise;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CrocDayOnePractiseApplicationTests {
 
     @Test
